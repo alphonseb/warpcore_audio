ef2f16ce4cf6b0f45fbe8c4f5b18a9d6c9162b13
Jules <jules.stalin@hetic.net>
2018-05-28 14:51:49 +0200

    Font

--- include/header.php
+++ include/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="styles/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
   </head>
   <body>
     <header>
