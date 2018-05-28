6c375b9d4f6f959a6b7caaa1046641692070541a
Jules <jules.stalin@hetic.net>
2018-05-28 14:42:19 +0200

    Header

---
+++ include/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="styles/style.css">
+  </head>
+  <body>
+    <header>
+      <nav>
+         <ul>
+           <li> <a href="#"> Accueil </a></li>
+           <li> <a href="#"> Professionnels </a></li>
+           <li> <a href="#"> Particuliers  </a></li>
+           <li> <a href="#"> L'artiste </a></li>
+           <li> <a href="#"> Contact </a></li>
+         </ul>
+      </nav>
+    </header>
