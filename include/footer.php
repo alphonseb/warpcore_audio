74d339745911809364c0f2e322bc28b71dd54b9e
alphonseb <alphonse.bouy@gmail.com>
2018-05-29 13:01:43 +0200

    moved stuff

--- include/footer.php
+++ include/footer.php
@@ -1,52 +1,30 @@
-<div class="avis">
-  <h2>Ils parlent de nous!</h2>
-</div>
-
-<div class="artiste">
-  <h2>L'Artiste</h2>
-  <h1>Mathieu Germain</h1>
-  <div class="presentation">
-    <span>"Je suis ingénieur du son et compositeur, spécialisé dans la production musicale"</span>
-    <ul class="logo_social">
-      <li><img src="images/soundcloud.png" alt=""></li>
-      <li><img src="images/facebook.png" alt=""></li>
-      <li><img src="images/mail.png" alt=""></li>
-      <li><img src="images/youtube.png" alt=""></li>
-    </ul>
-  </div>
-  <div class="know_more">
-    <span>En savoir plus</span>
-  </div>
-</div>
-
-<footer>
-  <div class="footer_text">
-    <h2>Nous contacter</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    <div class="contact phone">
-      <h3>Téléphone</h3>
-      <p>+33 6 34 53 23 43</p>
-    </div>
-    <div class="contact mail">
-      <h3>Email</h3>
-      <p>contact@warpcore.com</p>
-    </div>
-    <div class="contact network">
-      <h3>Réseaux sociaux</h3>
-      <ul class="logo_social">
-        <li><img src="images/soundcloud.png" alt=""></li>
-        <li><img src="images/facebook.png" alt=""></li>
-        <li><img src="images/mail.png" alt=""></li>
-        <li><img src="images/youtube.png" alt=""></li>
-      </ul>
-    </div>
-    <div class="contact adress">
-      <h3>Adresse</h3>
-      <p>7 adresse pas connue <br> 75009 Paris </p>
-    </div>
-  </div>
-  <img class="image_footer" src="images/wave.png" alt="">
-</footer>
-
-
-</body>
+    <footer>
+      <div class="footer_text">
+        <h2>Nous contacter</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div class="contact phone">
+          <h3>Téléphone</h3>
+          <p>+33 6 34 53 23 43</p>
+        </div>
+        <div class="contact mail">
+          <h3>Email</h3>
+          <p>contact@warpcore.com</p>
+        </div>
+        <div class="contact network">
+          <h3>Réseaux sociaux</h3>
+          <ul class="logo_social">
+            <li><img src="images/soundcloud.png" alt=""></li>
+            <li><img src="images/facebook.png" alt=""></li>
+            <li><img src="images/mail.png" alt=""></li>
+            <li><img src="images/youtube.png" alt=""></li>
+          </ul>
+        </div>
+        <div class="contact adress">
+          <h3>Adresse</h3>
+          <p>7 adresse pas connue <br> 75009 Paris </p>
+        </div>
+      </div>
+      <img class="image_footer" src="images/wave.png" alt="">
+    </footer>
+  </body>
+</html>
