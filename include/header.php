dea5ef3d923b997417feb3a9ca6822d9da68e869
alphonseb <alphonse.bouy@gmail.com>
2018-05-29 13:07:20 +0200

    update header

--- include/header.php
+++ include/header.php
@@ -26,8 +26,8 @@
            <li> <a href="#"> Contact </a><div class="underline"></div></li>
          </ul>
          <ul class="language">
-           <li><a href="#">FR | </a></li>
-           <li><a href="#" class="lg-selected"> EN</a></li>
+           <li><a href="#" class="lg-selected">FR | </a></li>
+           <li><a href="#"> EN</a></li>
          </ul>
       </nav>
       <!-- <div class="language">
