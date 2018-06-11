d0aa2420e1891df5fd76b866b895b3276d4a7764
Tichyus <32875615+Tichyus@users.noreply.github.com>
2018-06-11 19:37:33 +0200

    debut du formulaire

--- professional.php
+++ professional.php
@@ -2,11 +2,32 @@
 include "include/header.php";
 ?>
 
+    <main>
+        <div class="professional_icon">
+          <img src="images/people.png" alt="">
+          <h2>Professionels</h2>
+        </div>
+        <div class="form-container">
+          <h3>Demande de devis</h3>
+          <form class="quotation_form">
+            <div class="text_zone">
+              <div class="name_container">
+                <span>nom + prénom ou nom de la société</span>
+                <input type="text" name="name">
+              </div>
+              <div class="email_container">
+                <span>email</span>
+                <input type="text" name="name">
+              </div>
+              <div class="description_container">
+                <span>explication du projet</span>
+                <input class="description_input" type="text" name="name">
+              </div>
+            </div>
+          </form>
+        </div>
+    </main>
 
-
-<main>
-  <h1>Hello world!</h1>
-  <div class="professional_icon">
-
-  </div>
-</main>
+<?php
+include "include/footer.php";
+?>
